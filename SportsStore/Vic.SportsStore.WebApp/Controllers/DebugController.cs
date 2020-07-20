7236c8df4b9fdd2a9af8782851701fc16224fa16
Mark <Mark>
2020-07-20 18:27:06 -0400

    Adding DebugController and view page.

---
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/DebugController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class DebugController : Controller
+    {
+        // GET: Debug
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
