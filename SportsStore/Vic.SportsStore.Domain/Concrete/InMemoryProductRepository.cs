89e8e4bfaf247aaf973480a10eeaf3f965cfd3d9
Mark <Mark>
2020-07-20 21:16:44 -0400

    1. [CHG] Add product page.

---
+++ SportsStore/Vic.SportsStore.Domain/Concrete/InMemoryProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.Domain.Concrete
+{
+    public class InMemoryProductRepository : IProductsRepository
+    {
+        private List<Product> _products = new List<Product>
+        {
+            new Product { Name = "Football", Price =25},
+            new Product { Name = "Surf board", Price = 179},
+            new Product {Name="Running shoes", Price =95},
+            new Product { Name = "Snow board", Price = 309},
+            new Product {Name="Skin shoes", Price =195}
+        };
+         
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+    }
+}
---
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public IProductsRepository ProductsRepository { get; set; }
+            = new InMemoryProductRepository();
+
+       public ViewResult List()
+        {
+            var model = ProductsRepository.Products;
+            return View(model);
+        }
+    }
+}
