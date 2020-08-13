46b2262366dd92ead8aa9ff09fd3d15c12e02ddd
Mark <Mark>
2020-08-13 18:37:27 -0400

    1[CHG] Add shopping cart page.

--- SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
+++ SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
@@ -28,7 +28,7 @@ namespace Vic.SportsStore.WebApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
             null,
             "",
